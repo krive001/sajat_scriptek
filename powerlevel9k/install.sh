488690bddba5da3d91f01c2e687e7fe29a35619b
krive001 <krive001@gmail.com>
2019-02-24 18:18:56 +0100

    krive001

--- powerlevel9k/install.sh
+++ powerlevel9k/install.sh
@@ -1,28 +1,12 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 set -e
 
 # Install zsh
 
-sudo pacman -S zsh
+sudo pacman -S zsh --noconfirm --needed
 
 # oh-my-zsh install
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-# Powerlevel9k
-
-git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
-
-# Copy
-
-cp .zshrc ~
-sudo cp HackRegularNerdFontComplete.ttf /usr/share/fonts/TTF/
-sudo cp Hack\ Regular\ Nerd\ Font\ Complete\ Mono.ttf /usr/share/fonts/TTF/
-cp icons.zsh ~/.oh-my-zsh1/custom/themes/powerlevel9k/functions/
-
-# Refresh font cache
-
-sudo fc-cache -fv
-
-# Set terminal font Hack Nerd Font Regular
