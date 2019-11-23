e92a6e7273d13b594744bafc0f70dc6dec1e5817
krive001 <krive001@gmail.com>
2019-11-23 17:34:58 +0100

    aaa

--- powerlevel9k/install2.sh
+++ powerlevel9k/install2.sh
@@ -2,23 +2,25 @@
 
 # Powerlevel9k
 
-git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+# git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+
 
 # Copy
 
 cp .zshrc ~
-cp .Xresources ~
-sudo cp HackRegularNerdFontComplete.ttf /usr/share/fonts/TTF/
-sudo cp Hack\ Regular\ Nerd\ Font\ Complete\ Mono.ttf /usr/share/fonts/TTF/
-cp icons.zsh ~/.oh-my-zsh/custom/themes/powerlevel9k/functions/
+#cp .Xresources ~
+# sudo cp HackRegularNerdFontComplete.ttf /usr/share/fonts/TTF/
+#sudo cp Hack\ Regular\ Nerd\ Font\ Complete\ Mono.ttf /usr/share/fonts/TTF/
+#cp icons.zsh ~/.oh-my-zsh/custom/themes/powerlevel9k/functions/
 
 # Refresh font cache
 
-sudo fc-cache -fv
+#sudo fc-cache -fv
 
 # Set terminal font Hack Nerd Font Regular
 
-mkdir ~/.config/mc
+#mkdir ~/.config/mc
 
 cp mc/* ~/.config/mc
 
